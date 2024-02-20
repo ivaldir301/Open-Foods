33b0a8ce1d541ea8bc6b888de439956aa08571e0
Ivaldir Batalha <ivalbat1@gmail.com>
2024-02-20 12:15:13 -0100

    new url to the pyppeteer configurations file

---
+++ pyppeteer_scripts/configuration/pyppeteerConfigurations.py
@@ -0,0 +1,15 @@
+pyppeteer_configurations = {
+    'browserConfigurations': {
+        'browserName': 'chrome',
+        'browserVersion': 'latest',
+        'headless': 1,
+        'args': '--start-maximized'
+    },
+    
+    'websiteConfigurations': {
+        'url': 'https://br.openfoodfacts.org/nova-group/1-alimentos-nao-processados-ou-minimamente-processados?sort_by=nutriscore_score'
+    },
+    
+    'proxyConfigurations': {
+    }
+}
