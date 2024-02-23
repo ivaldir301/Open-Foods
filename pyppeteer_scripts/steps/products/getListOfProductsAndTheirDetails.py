c3455200378ab9f735c1b7d1d44998b204c0a9de
Ivaldir Batalha <ivalbat1@gmail.com>
2024-02-23 22:41:02 -0100

    Refactored some files a little bit, variable names concistency

--- pyppeteer_scripts/steps/products/getListOfProductsAndTheirDetails.py
+++ pyppeteer_scripts/steps/products/getListOfProductsAndTheirDetails.py
@@ -13,54 +13,54 @@ logging.getLogger().addHandler(logging.StreamHandler())
 async def getListOfProductsAndTheirDetails(pageReference: any):
     # wait for the #products_match_all to load and store it in a variable
     await pageReference.waitForSelector("#products_match_all")
-    product_elements = await pageReference.querySelectorAll("#products_match_all li")
+    productElements = await pageReference.querySelectorAll("#products_match_all li")
     
     logging.info("----------> Saving all products found ...")
         
     # going throught all the li elements that contains each product and picking the needed information
     listOfAllProducts = []
-    for product_element in product_elements:
+    for productElements in product_elements:
         logging.info("----------> Parsing all products before returning ...")
-        product_info = {}
+        productInfo = {}
         
         try:
-            product_info['id'] = await product_element.querySelectorEval('.list_product_a', '(element) => element.getAttribute("href")')
-            product_info['id'] = formatProductId(product_info['id'])
+            productInfo['id'] = await productElements.querySelectorEval('.list_product_a', '(element) => element.getAttribute("href")')
+            productInfo['id'] = formatProductId(productInfo['id'])
         except:
             logging.warning("----------> Wasn't able to get the product id")
             
         try:
-            product_info['name'] = await product_element.querySelectorEval('.list_product_name', '(element) => element.textContent')
-            product_info['name'] = formatProductName(product_info['name'])
+            productInfo['name'] = await productElements.querySelectorEval('.list_product_name', '(element) => element.textContent')
+            productInfo['name'] = formatProductName(productInfo['name'])
         except:
             logging.warning("-----------> Wasn't able to get the product name")
         
         # going through all the images inside the .list_product_sc div and picking all the titles containing information about nutrition and nova
         try:
-            product_scores_element = await product_element.querySelector('.list_product_sc')
+            productScoresElement = await productElements.querySelector('.list_product_sc')
         except:
             logging.warning("----------> There was a problem while getting the list of products component")
         
         try:
-            product_score_icons = await product_scores_element.querySelectorAll('.list_product_icons')
+            productScoreIcons = await productScoresElement.querySelectorAll('.list_product_icons')
         except:
             logging.warning("----------> There was a problem getting the list of images")
             
-        product_info['nutrition'] = {}
-        product_info['nova'] = {}
+        productInfo['nutrition'] = {}
+        productInfo['nova'] = {}
         i: int = 0
-        for icon in product_score_icons:
+        for icon in productScoreIcons:
             try:
                 title = await pageReference.evaluate('(element) => element.getAttribute("title")', icon)
                 title = formatNutritionAndNovaDetails(title)
             except:
                 logging.warning("----------> There was a problem getting the title")
             if i == 0:
-                product_info['nutrition'][f'score'] = formatProductNutriScore(title)
-                product_info['nutrition'][f'title'] = setProductNutriScoreTitle(title)
+                productInfo['nutrition'][f'score'] = formatProductNutriScore(title)
+                productInfo['nutrition'][f'title'] = setProductNutriScoreTitle(title)
             elif i == 1:
-                product_info['nova'][f'score'] = formatProductNovaScore(title)
-                product_info['nova'][f'title'] = setProductNovaScoreTitle(title)
+                productInfo['nova'][f'score'] = formatProductNovaScore(title)
+                productInfo['nova'][f'title'] = setProductNovaScoreTitle(title)
             i += 1
-        listOfAllProducts.append(product_info)
+        listOfAllProducts.append(productInfo)
     return listOfAllProducts
